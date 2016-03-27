11ad8ee98c16e6ba0320fb84c99d56c424e251f8
Gaurav Kumar <gauravbansal74@gmail.com>
2016-03-27 08:15:27 +0000

    new file added

--- requirejs_with_jquery/js/app.js
+++ requirejs_with_jquery/js/app.js
@@ -1,10 +1,23 @@
+// Place third party dependencies in the lib folder
+//
+// Configure loading modules from the lib directory,
+// except 'app' ones, 
 requirejs.config({
-    "baseUrl": "js/lib",
-    "paths": {
-      "app": "../app"
-    },
-    "shim": {
-        "jquery": ["jquery"]
+    baseUrl: 'js/lib',
+    paths: {
+        // the left side is the module ID,
+        // the right side is the path to
+        // the jQuery file, relative to baseUrl.
+        // Also, the path should NOT include
+        // the '.js' file extension. This example
+        // is using jQuery 1.9.0 located at
+        // js/lib/jquery-1.9.0.js, relative to
+        // the HTML page.
+        jquery: 'jquery',
+        "app": "../app"
+        
     }
 });
+
+// Load the main app module to start the app
 requirejs(["app/main"]);
